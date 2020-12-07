4a8a4b8dec0f8d3fb0a6650fc8dd9ad2720c7a99
cjw5xd <6518663+cjw5xd@users.noreply.github.com>
2020-12-07 11:20:47 -0800

    Use variable for address prefix

--- paperbtc.py
+++ paperbtc.py
@@ -6,6 +6,10 @@ import base58
 import ecdsa
 
 
+# Using the 0x00 prefix specifies the P2PKH locking script.
+PREFIX = b'\x00'
+
+
 def sha256(data):
     return hashlib.sha256(data).digest()
 
@@ -22,12 +26,11 @@ if __name__ == '__main__':
     # Calculate the public key hash, also known as the HASH160.
     public_key_hash = ripemd160(sha256(public_key._compressed_encode()))
 
-    # Calculate the checksum for the extended public key hash; the 0x00 prefix
-    # specifies the P2PKH locking script.
-    checksum = sha256(sha256(b'\x00' + public_key_hash))[:4]
+    # Calculate the checksum for the extended public key hash.
+    checksum = sha256(sha256(PREFIX + public_key_hash))[:4]
 
     # Determine the address in base58 format.
-    address = base58.b58encode(b'\x00' + public_key_hash + checksum).decode()
+    address = base58.b58encode(PREFIX + public_key_hash + checksum).decode()
 
     print('Address:', address)
     print('Public Key:', public_key._compressed_encode().hex())
