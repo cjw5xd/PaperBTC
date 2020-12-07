f10b7a899f669c9c8e4446c83bf1216269ce3ab7
cjw5xd <6518663+cjw5xd@users.noreply.github.com>
2020-12-07 11:18:57 -0800

    Add check for __main__

--- paperbtc.py
+++ paperbtc.py
@@ -14,21 +14,22 @@ def ripemd160(data):
     return hashlib.new('ripemd160', data).digest()
 
 
-# Generate an ECDSA key pair.
-private_key = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)
-public_key = private_key.get_verifying_key()
+if __name__ == '__main__':
+    # Generate an ECDSA key pair.
+    private_key = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)
+    public_key = private_key.get_verifying_key()
 
-# Calculate the public key hash, also known as the HASH160.
-public_key_hash = ripemd160(sha256(public_key._compressed_encode()))
+    # Calculate the public key hash, also known as the HASH160.
+    public_key_hash = ripemd160(sha256(public_key._compressed_encode()))
 
-# Calculate the checksum for the extended public key hash; the 0x00 prefix
-# specifies the P2PKH locking script.
-checksum = sha256(sha256(b'\x00' + public_key_hash))[:4]
+    # Calculate the checksum for the extended public key hash; the 0x00 prefix
+    # specifies the P2PKH locking script.
+    checksum = sha256(sha256(b'\x00' + public_key_hash))[:4]
 
-# Determine the address in base58 format.
-address = base58.b58encode(b'\x00' + public_key_hash + checksum).decode()
+    # Determine the address in base58 format.
+    address = base58.b58encode(b'\x00' + public_key_hash + checksum).decode()
 
-print('Address:', address)
-print('Public Key:', public_key._compressed_encode().hex())
-print('Public Key Hash:', public_key_hash.hex())
-print('Private Key:', private_key.to_string().hex())
+    print('Address:', address)
+    print('Public Key:', public_key._compressed_encode().hex())
+    print('Public Key Hash:', public_key_hash.hex())
+    print('Private Key:', private_key.to_string().hex())
